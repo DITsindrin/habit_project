47adefe6bef729f043e6eff458645a21d375b27c
ditsindrin <dmitriytsindrin1@gmail.com>
2024-03-13 00:09:14 +0300

    Create models Habit. Add serializer Habit. Add CRUD Habit. Add urls Habit.

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
+PERIODICITY_CHOICES = (
+    (1, 'раз в день'),
+    (2, 'раз в два дня'),
+    (3, 'раз в три дня'),
+    (4, 'раз в четыре дня'),
+    (5, 'раз в пять дней'),
+    (6, 'раз в шесть дней'),
+    (7, 'раз в семь дней')
+)
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Создатель привычки')
+    place = models.CharField(max_length=150, verbose_name='Место выполнения привычки')
+    time = models.TimeField(verbose_name='Время, когда необходимо выполнять привычку')
+    action = models.CharField(max_length=150, verbose_name='Действие')
+    sign_pleasant_habit = models.BooleanField(default=False, verbose_name='Признак приятной привычки')
+    related_habit = models.ForeignKey('habits.Habit', on_delete=models.SET_NULL, verbose_name='Связанная привычка',
+                                      null=True, blank=True)
+    periodicity = models.IntegerField(choices=PERIODICITY_CHOICES, verbose_name='Периодичность выполнения привычки')
+    reward = models.CharField(max_length=150, verbose_name='Вознаграждение', null=True, blank=True)
+    time_complete = models.TimeField(verbose_name='Время на выполнение привычки')
+    sign_publicity = models.BooleanField(default=False, verbose_name='Признак публичности')
+
+    def __str__(self):
+        return f'Я {self.user} буду {self.action} {self.periodicity} в {self.time} - {self.place}'
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
+        ordering = ('id',)
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
 
 # Create your views here.
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """ Вывод списка привычек """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """ Создание списка привычки """
+    serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.user = self.request.user
+        habit.save()
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """ Просмотр одной привычки """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """ Изменение привычки """
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """ Удаление привычки """
+    queryset = Habit.objects.all()
